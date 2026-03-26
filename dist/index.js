970de9f98b7673fe2b623c0f387a3c58f831d263
Jovanni099 <jovanni099tmb@gmail.com>
2026-03-26 10:31:24 -0700

    prepare project for prisma

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,5 @@
-import express from "express";
-const app = express();
+import app from "./app";
 const PORT = 3000;
-app.use(express.json());
-app.get("/health", (_req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Server is running",
-    });
-});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-//# sourceMappingURL=index.js.map
