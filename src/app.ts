d12e3f9790f7677762d1919a96bf8b5f9be6e979
Jovanni099 <jovanni099tmb@gmail.com>
2026-03-26 12:25:55 -0700

    connect prisma client

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from "express";
-
-import healthRouter from "./routes/health.routes";
+import healthRouter from "./routes/health.routes.js";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(healthRouter);
 
 export default app;
