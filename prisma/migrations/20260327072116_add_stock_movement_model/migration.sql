ad06a24924659884bc40474d80e75aa0cd077ed9
Jovanni099 <jovanni099tmb@gmail.com>
2026-03-27 00:22:37 -0700

    feat: add stock movement model

---
+++ prisma/migrations/20260327072116_add_stock_movement_model/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "MovementType" AS ENUM ('IN', 'OUT');
+
+-- CreateTable
+CREATE TABLE "StockMovement" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "warehouseId" TEXT NOT NULL,
+    "type" "MovementType" NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "note" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "StockMovement_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "StockMovement" ADD CONSTRAINT "StockMovement_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StockMovement" ADD CONSTRAINT "StockMovement_warehouseId_fkey" FOREIGN KEY ("warehouseId") REFERENCES "Warehouse"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
